2fcd9f951a2767ea333d9a98042c08bb5727193f
assassin_box <assassin543@outlook.jp>
2022-05-29 12:41:19 +0900

    sql修正

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -9,6 +9,22 @@ CREATE FUNCTION update_updated_at_column()
   END;
   $$ language 'plpgsql';
 
+-- Master Tabel
+-- shrine
+CREATE TABLE shrine (
+  id SERIAL NOT NULL,
+  name varchar(64) NOT NULL DEFAULT '',
+  created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  PRIMARY KEY (id)
+);
+
+-- マスターデータ
+INSERT INTO shrine (name) VALUES 
+('橿原神宮'), 
+('鶴岡八幡宮'), 
+('品川神社');
+
 -- users
 CREATE TABLE users (
   id SERIAL NOT NULL,
@@ -31,15 +47,14 @@ CREATE TRIGGER update_user_modtime
 EXECUTE PROCEDURE update_updated_at_column();
 
 -- サンプル
-INSERT INTO users (name, email, pass) VALUES
-('sam', 'sample01@example.com', 'sample01')
-;
+INSERT INTO users (id, name, email, pass) VALUES 
+(1, 'sam', 'sample01@example.com', 'sample01');
 
 -- photos
 CREATE TABLE photos (
   id SERIAL NOT NULL,
-  user_id varchar(64) NOT NULL DEFAULT '',
-  shrine_id varchar(64) NOT NULL DEFAULT '',
+  user_id int NOT NULL DEFAULT 0,
+  shrine_id int NOT NULL DEFAULT 0,
   url varchar(200) NOT NULL DEFAULT '',
   impression varchar(200) NOT NULL DEFAULT '',
   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
@@ -60,17 +75,16 @@ CREATE TRIGGER update_photos_modtime
 EXECUTE PROCEDURE update_updated_at_column();
 
 -- サンプル
-INSERT INTO photos (url, impression, user_id, shrine_id) VALUES
-('sample1.com', '故郷', 1, 1),
-('sample2.com', '感謝', 1, 2),
-('sample3.com', '階段', 1, 3)
-;
+INSERT INTO photos (url, impression, user_id, shrine_id) VALUES 
+('sample1.com', '故郷', 1, 1), 
+('sample2.com', '感謝', 1, 2), 
+('sample3.com', '階段', 1, 3);
 
 -- emo
 CREATE TABLE emo (
   id SERIAL NOT NULL,
-  user_id varchar(64) NOT NULL DEFAULT '',
-  photos_id varchar(64) NOT NULL DEFAULT '',
+  user_id int NOT NULL DEFAULT 0,
+  photos_id int NOT NULL DEFAULT 0,
   emo varchar(20) NOT NULL DEFAULT '',
   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
   updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
@@ -90,27 +104,8 @@ CREATE TRIGGER update_emo_modtime
 EXECUTE PROCEDURE update_updated_at_column();
 
 -- サンプル
-INSERT INTO emo (emo, user_id, photos_id) VALUES
-('♥', 1, 1),
-('♥', 1, 2),
-('♥', 2, 1),
-('♥', 3, 3)
-;
-
-
--- Master Tabel
--- shrine
-CREATE TABLE shrine (
-  id SERIAL NOT NULL,
-  name varchar(64) NOT NULL DEFAULT '',
-  created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
-  updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
-  PRIMARY KEY (id)
-);
-
--- マスターデータ
-INSERT INTO shrine (name) VALUES
-('橿原神宮'),
-('鶴岡八幡宮'),
-('品川神社')
-;
+INSERT INTO emo (emo, user_id, photos_id) VALUES 
+('a', 1, 1), 
+('a', 1, 2), 
+('a', 2, 1), 
+('a', 3, 3);
