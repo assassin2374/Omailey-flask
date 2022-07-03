64c93537b61abb49efacfde8c61671bfabed1d49
assassin_box <assassin543@outlook.jp>
2022-07-04 01:31:21 +0900

    API追加 #4

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, request
+from flask import request
 from application import app
 from models.user import User
 
@@ -9,8 +9,22 @@ from models.user import User
 def query_users():
     users = User.query.all()
     print(users[0].to_dict())
-    id = request.args.get("id")
+    id = request.args.get('id')
     return users[0].to_dict()
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    result = json.loads(request.data)
+    return result
+
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id = None):
+    return 'update' + str(user_id)
+
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id = None):
+    return 'delete' + str(user_id)
+
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
