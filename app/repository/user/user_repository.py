50cf8f966888b5fec1d3f7881959a739ba1467b8
assassin_box <assassin543@outlook.jp>
2022-09-04 03:26:52 +0900

    名称変更

--- app/repository/user/user_repository.py
+++ app/repository/user/user_repository.py
@@ -1,9 +1,5 @@
 
 
-
-@app.route('/users', methods=['GET'])
 def query_users():
     users = User.query.all()
-    print(users[0].to_dict())
-    id = request.args.get('id')
     return users[0].to_dict()
